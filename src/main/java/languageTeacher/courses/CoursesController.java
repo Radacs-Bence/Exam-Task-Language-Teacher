d5d24b4cdeda12e61c8c0dc9a432bdde41cabc38
Radacs-Bence <bence.radacs@gmail.com>
2021-08-06 00:16:02 +0200

    Swagger operációk összegzése és leírása.

--- src/main/java/languageTeacher/courses/CoursesController.java
+++ src/main/java/languageTeacher/courses/CoursesController.java
@@ -1,6 +1,9 @@
 package languageTeacher.courses;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import languageTeacher.Languages;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@Tag( name = "Operations on courses")
 public class CoursesController {
 
     private CoursesService coursesService;
@@ -19,22 +23,30 @@ public class CoursesController {
     }
 
     @GetMapping
+    @Operation(summary = "Find all courses",
+            description = "Lists the courses in the repository.")
     public List<CourseDTO> listCourses(){
         return coursesService.listCourses();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find course by id",
+            description = "Finds the course based on id, without timetable.")
     public CourseDTO findCourseById(@PathVariable("id") Long id){
         return coursesService.findCourseById(id);
     }
 
     @GetMapping("/{id}/timetable")
+    @Operation(summary = "Find course by id, with timetable",
+            description = "Finds the course based on id with timetable, but without Teacher.")
     public CourseTimetableDTO findCourseTimetable(@PathVariable("id") Long id){
         return coursesService.findCourseByIdWithTimetable(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Creates a course",
+            description = "Creates a course, with name and language.")
     public CourseDTO saveCourse(@RequestBody CreateCourseCommand command){
         return coursesService.saveCourse(command);
     }
@@ -42,36 +54,48 @@ public class CoursesController {
 
     @PostMapping("/{id}/timetable")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Adds a timeslot to the course",
+            description = "Makes a timeslot and adds it to the course.")
     public CourseTimetableDTO addTimeslot(@PathVariable("id") Long id, @RequestBody CreateTimeslotCommand command){
         return coursesService.addTimeslot(id, command);
     }
 
     @PutMapping("/{id}/duration")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Find course by id, with timetable",
+            description = "Finds the course based on id with timetable, but without Teacher.")
     public CourseDTO modifyDuration(@PathVariable Long id, @RequestBody UpdateDurationCommand command){
         return coursesService.modifyDuration(id, command);
     }
 
     @PutMapping("/{id}/teacher/{teacherId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Change the teacher of the course",
+            description = "Changes the teacher for the course, removing it from the old one.")
     public CourseDTO modifyTeacher(@PathVariable Long id, @PathVariable Long teacherId){
         return coursesService.modifyTeacher(id, teacherId);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete course",
+            description = "Deletes the course by id.")
     public void deleteCourse(@PathVariable Long id){
         coursesService.deleteCourse(id);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete all courses",
+            description = "DELETES ALL COURSES! USE CAREFULLY!")
     public void deleteAll(){
         coursesService.deleteAll();
     }
 
     @DeleteMapping ("/{id}/timetable/{timeSlotId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Remove timeslot",
+            description = "Removes a timeslot frm the course and deletes it.")
     public void removeTimeslot(@PathVariable("id") Long id , @PathVariable("timeSlotId") Long timeSlotId){
         coursesService.removeTimeslot(id, timeSlotId);
     }
--- src/main/java/languageTeacher/teachers/TeachersController.java
+++ src/main/java/languageTeacher/teachers/TeachersController.java
@@ -1,5 +1,6 @@
 package languageTeacher.teachers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import languageTeacher.courses.CreateCourseCommand;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,58 +18,78 @@ public class TeachersController {
     }
 
     @GetMapping
+    @Operation(summary = "Find all teachers",
+            description = "Finds all teachers in repository, does not list courses of the teachers.")
     public List<TeacherDTO> listTeachers(){
         return teachersService.listTeachers();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find teachers by id",
+            description = "Finds a teacher in the repository, does not list courses of the teacher.")
     public TeacherDTO findTeacherById(@PathVariable("id") Long id){
         return teachersService.findTeacherById(id);
     }
 
     @GetMapping("/{id}/courses")
+    @Operation(summary = "Find teachers by id with courses",
+            description = "Finds a teacher in the repository, with the id and name of the course.")
     public TeacherCourseDTO findTeacherByIdWithCourses(@PathVariable("id") Long id){
         return teachersService.findTeacherByIdWithCourses(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Save a teacher",
+            description = "Saves a teacher into the repository with a name.")
     public TeacherDTO saveTeacher(@RequestBody CreateTeacherCommand command){
         return teachersService.saveTeacher(command);
     }
 
     @PostMapping("/{id}/languages")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Add language to teacher",
+            description = "Adds a language to the teacher.")
     public TeacherDTO addLanguage(@PathVariable Long id, @RequestBody UpdateLanguageCommand command){
         return teachersService.addLanguage(id, command);
     }
 
     @PutMapping("/{id}/contact")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Modify contact info",
+            description = "Modifys the contact information of the teacher.")
     public TeacherDTO modifyContact(@PathVariable Long id, @RequestBody UpdateContactCommand command){
         return teachersService.modifyContact(id, command);
     }
 
     @PutMapping("/{id}/courses")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(summary = "Add course to teacher",
+            description = "Adds a newly created course to the teacher.")
     public TeacherCourseDTO addCourse(@PathVariable Long id, @RequestBody CreateCourseCommand command){
         return teachersService.addCourse(id, command);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete teacher by id",
+            description = "Deletes the teacher form the database.")
     public void deleteTeacher(@PathVariable Long id){
         teachersService.deleteTeacher(id);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete all teachers",
+            description = "DELETES ALL TEACHERS! USE CAREFULLY!")
     public void deleteAll(){
         teachersService.deleteAll();
     }
 
     @DeleteMapping("/{id}/courses/{courseId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Remove course from teacher",
+            description = "Removes the course from the teacher, however it's not deleted, it can get a different teacher.")
     public void removeTeacherCourse(@PathVariable Long id, @PathVariable Long courseId){
         teachersService.removeTeacherCourse(id, courseId);
     }
